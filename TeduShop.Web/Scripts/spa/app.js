0ee649e78ba2ea76f4e5333d8050660c0c83a2c7
Mai Thành Duy An <tiachop0102@gmail.com>
2018-05-15 22:08:45 +0700

    create Directive in AngularJS

--- TeduShop.Web/Scripts/spa/app.js
+++ TeduShop.Web/Scripts/spa/app.js
@@ -3,22 +3,32 @@
 var myApp = angular.module('myModule', []);
 
 myApp.controller("schoolController", schoolController);
-myApp.service("validator", validator);
-schoolController.$inject = ['$scope', 'validator'];
+myApp.service("validatorService", validatorService);
+myApp.directive("teduShopDirective", teduShopDirective);
 
-function schoolController($scope, validator) {
+schoolController.$inject = ['$scope', 'validatorService'];
+
+function schoolController($scope, validatorService) {
     $scope.checkNumber = function () {
-        $scope.message = validator.checkNumber($scope.num);
+        $scope.message = validatorService.checkNumber($scope.num);
     }
     $scope.num = 1;
 }
 
-function validator($window) {
+function validatorService($window) {
     return { checkNumber: checkNumber }
     function checkNumber(input) {
         if (input % 2 == 0) {
             return 'This is even';
         }
-        else return 'This is odd';
+        else
+            return 'This is odd';
+    }
+}
+
+function teduShopDirective() {
+    return {
+        restrict:"E",
+        templateUrl:"/Scripts/spa/teduShopDirective.html" 
     }
 }
